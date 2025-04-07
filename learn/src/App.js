30c019b717413efe344f780732fc40369c0a8db5
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2025-04-06 21:09:14 -0300

    Remoção de componentes não utilizados da aplicação

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,34 +1,10 @@
 import React from "react"
 
-const Abalt = (entrada) => {
-  return (
-    <div>
-
-      <h2>Nome: {entrada.nome}</h2>
-      <h3>Idade: {entrada.idade}</h3>
-      <h3>Cargo: {entrada.cargo}</h3>
-      <hr/>
-    </div>
-  )
-}
-
-
-
-const Equipe = (entrada) => {
-  return (
-    <div>
-      <Abalt nome = "Vinicius Pessoa" idade = "22" cargo = "Vagabundo"/>
-      <Abalt nome = "Edivan Fodão" idade = "43" cargo = "Adiministrador"/>
-    </div>
-  )
-}
-
 export default function App() {
   return (
     <div>
       <h1>Welcome to the React App</h1>
       <hr></hr>
-      <Equipe></Equipe>
     </div>
   );
 }
