a90501d06ac1be462161927cc84e7cdba6bb53af
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2025-04-09 01:44:49 -0300

    requisição

--- movieproject/src/pages/Home/index.js
+++ movieproject/src/pages/Home/index.js
@@ -1,44 +1,33 @@
-import { useEffect, useState } from "react";
-import api from "../../services/api";
+import { useEffect, useState} from 'react';
+import api from '../../services/api';
 
-function Home() {
-    const [filmes, setFilmes] = useState([]);
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
-    useEffect(() => {
-        async function loadFilmes() {
-            const response = await api.get("movie/now_playing", {
-                params: {
-                    api_key: "2dec5b8f560d2fe7a18ca46bdad98949",
-                    language: "pt-BR",
-                    page: 1,
-                }
-            });
-            
-            setFilmes(response.data.results); // Atualiza o estado com os filmes
+
+function Home(){
+  // const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=>{
+
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params:{
+         api_key: "2dec5b8f560d2fe7a18ca46bdad98949",
+         language: "pt-BR",
+         page: 1,
         }
+      })
+    }
 
-        loadFilmes();
-    }, []); // Array de dependências vazio para executar apenas uma vez
+    loadFilmes();
 
-    return (
-        <div>
-            <h1>Página Home</h1>
-            {/* Exemplo de renderização dos filmes */}
-            <ul>
-                {filmes.map((filme) => (
-                    <li key={filme.id}>
-                        <h2>{filme.title}</h2>
-                        <img 
-                            src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`} 
-                            alt={filme.title}
-                            width="200"
-                        />
-                        <p>{filme.overview}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  })
+
+  return(
+    <div>
+      <h1>BEM VINDO A HOME</h1>
+    </div>
+  )
 }
 
 export default Home;
--- movieproject/src/services/api.js
+++ movieproject/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 //  /movie/now_playing?api_key=2dec5b8f560d2fe7a18ca46bdad98949&language=pt-BR
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
+    baseURL: 'https://api.themoviedb.org/3/'
 });
 
 export default api;
