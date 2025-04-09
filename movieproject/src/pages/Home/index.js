fa0f8f7791f8e0f8a74f84139b074c175110dc6e
ViniciusBPessoa <viniciusbpessoa02@gmail.com>
2025-04-09 01:27:06 -0300

    reaquisição de API em estado avançado
    reaquisição de API em estado avançado

--- movieproject/src/pages/Home/index.js
+++ movieproject/src/pages/Home/index.js
@@ -1,10 +1,44 @@
-function Home(){
-    return(
-        <div>
-            <h1>Pagina Home</h1>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+import api from "../../services/api";
 
+function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "2dec5b8f560d2fe7a18ca46bdad98949",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            });
+            
+            setFilmes(response.data.results); // Atualiza o estado com os filmes
+        }
+
+        loadFilmes();
+    }, []); // Array de dependências vazio para executar apenas uma vez
+
+    return (
+        <div>
+            <h1>Página Home</h1>
+            {/* Exemplo de renderização dos filmes */}
+            <ul>
+                {filmes.map((filme) => (
+                    <li key={filme.id}>
+                        <h2>{filme.title}</h2>
+                        <img 
+                            src={`https://image.tmdb.org/t/p/w500${filme.poster_path}`} 
+                            alt={filme.title}
+                            width="200"
+                        />
+                        <p>{filme.overview}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default Home;
