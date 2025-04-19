aa95426cc3a612528483022c1deee442262ad7e2
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2025-04-19 19:19:40 -0300

    Inicialização do perfil finalizado
    Inicialização do perfil finalizado
    
    Perfil inicial, descrição e redes sociais finalizadas.

--- minhapagina/src/components/Header/index.js
+++ minhapagina/src/components/Header/index.js
@@ -1,14 +1,39 @@
-import "./header.css";
+import React from 'react';
+import './header.css'; // Importa o CSS local
 
-import { Link } from "react-router-dom";
+// Importa as imagens da pasta local 'imgs'
+import linkedin from './imgs/linkedin.png';
+import gitHub from './imgs/gitHub.png';
+import email from './imgs/email.png';
 
-function Header(){
-    return(
-        <header>
-            <Link className="logo" to={"/"}>Sad Flix</Link>
-            <Link className="favoritos" to={"/favoritos"}>Meus Filmes</Link>
-        </header>
-    )
-}
+const Header = () => {
+  return (
+    <header className="header-container">
+      {/* Ícones das redes sociais (lado esquerdo) */}
+      <div className="social-icons">
+        <a href="https://www.linkedin.com/in/vinicius-bezerra-b0920b284/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedin} alt="linkedin" className="social-icon" />
+        </a>
+        <a href="https://github.com/ViniciusBPessoa" target="_blank" rel="noopener noreferrer">
+          <img src={gitHub} alt="github" className="social-icon" />
+        </a>
+        <a 
+        href="mailto:contato@vinicius.com" 
+        title="Enviar e-mail"
+        aria-label="Enviar e-mail"
+        target="_blank" 
+        rel="noopener noreferrer"
+        >
+        <img src={email} alt="Ícone de e-mail" className="social-icon" />
+      </a>
+      </div>
+
+      {/* Texto "Inglês" (lado direito) */}
+      <div className="language-text">
+        <p>Inglês</p>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
